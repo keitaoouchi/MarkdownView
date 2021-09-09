45d6dbd9fff92a233621b3eb570a7f39f65f6435
keita.oouchi <keita.oouchi@gmail.com>
2021-09-09 23:34:36 +0900

    Refactoring

--- Example/Example/ViewController/CodeOnly.swift
+++ Example/Example/ViewController/CodeOnly.swift
@@ -1,8 +1,17 @@
 import UIKit
 import MarkdownView
 
-class Example1ViewController: UIViewController {
-
+class CodeOnlySampleViewController: UIViewController {
+  
+  init() {
+    super.init(nibName: nil, bundle: nil)
+    view.backgroundColor = .systemBackground
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
